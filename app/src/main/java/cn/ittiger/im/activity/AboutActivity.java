788a62ee090cc08ed12c53e49140d3e1ac818a70
ylhu <ylhu@bainainfo.com>
2017-02-23 17:26:30 +0800

    完善关于页面

--- app/src/main/java/cn/ittiger/im/activity/AboutActivity.java
+++ app/src/main/java/cn/ittiger/im/activity/AboutActivity.java
@@ -4,13 +4,18 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ittiger.im.R;
-import cn.ittiger.im.activity.base.IMBaseActivity;
 import cn.ittiger.util.ActivityUtil;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -19,11 +24,13 @@ import android.view.View;
  * @author: laohu on 2016/12/27
  * @site: http://ittiger.cn
  */
-public class AboutActivity extends IMBaseActivity {
+public class AboutActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
     @BindView(R.id.collapsing_toolbar)
     CollapsingToolbarLayout mCollapsingToolbarLayout;
+    @BindView(R.id.about_nestedScrollView)
+    NestedScrollView mNestedScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,15 +55,25 @@ public class AboutActivity extends IMBaseActivity {
     @OnClick({R.id.tv_about_blog, R.id.tv_about_github})
     public void onClick(View view) {
 
-        String url;
-        if(view.getId() == R.id.tv_about_blog) {
-            url = "http://ittiger.cn";
-        } else {
-            url = "https://github.com/huyongli/TigerIM";
+        String url = null;
+        switch (view.getId()) {
+            case R.id.tv_about_blog:
+                url = "http://ittiger.cn";
+                break;
+            case R.id.tv_about_github:
+                url = "https://github.com/huyongli/TigerIM";
+                break;
+            case R.id.tv_about_contact:
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                clipboard.setPrimaryClip(ClipData.newPlainText("text", "106510493"));
+                Snackbar.make(mNestedScrollView, "复制成功", Snackbar.LENGTH_SHORT).show();
+                break;
         }
-        Intent intent = new Intent(mActivity, WebPageActivity.class);
-        Uri content_url = Uri.parse(url);
-        intent.setData(content_url);
-        ActivityUtil.startActivity(mActivity, intent);
+        if(url != null) {
+            Intent intent = new Intent(this, WebPageActivity.class);
+            Uri content_url = Uri.parse(url);
+            intent.setData(content_url);
+            ActivityUtil.startActivity(this, intent);
+        }
     }
 }
